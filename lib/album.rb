ea820df40186d4105462f2b44628780af8ee590c
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-04 21:00:41 -0700

    4 tests passing.

--- lib/album.rb
+++ lib/album.rb
@@ -1,11 +1,29 @@
 class Album
+  attr_reader :id, :name
+  
   @@albums = {}
 
-  def initialize(name)
+  @@total_rows = 0
+
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1
   end
 
   def self.all
     @@albums.values()
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
   end
 end
